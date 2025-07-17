b66aedc3a4879880169528d9befd094f1efd09ab
Muse Tools <musetools.uk@gmail.com>
2025-07-17 11:20:01 +0800

    :tada: init bookkeeping project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Muse Bookkeeping
+
+小项目，记账本
+
+## License
+
+MIT
---
+++ apps/muse_bookkeeping/android/app/src/main/kotlin/com/example/muse_bookkeeping/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.muse_bookkeeping
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
